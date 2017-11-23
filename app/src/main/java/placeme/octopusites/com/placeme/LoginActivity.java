6e10cb8803a577a79dbd05fc899f1ccc36ad3470
kunal <kunalkhedkar10@gmail.com>
2017-11-23 11:38:16 +0530

    kunal signup call

--- app/src/main/java/placeme/octopusites/com/placeme/LoginActivity.java
+++ app/src/main/java/placeme/octopusites/com/placeme/LoginActivity.java
@@ -181,8 +181,8 @@ public class LoginActivity extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-
+//                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+                startActivity(new Intent(LoginActivity.this, Welcome.class));
             }
         });
 
